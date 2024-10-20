90b695b1149533846d0624d3e2ba9af20e3ba39a
ps <pio.szo@proton.me>
2024-10-20 11:40:54 +0200

    update configs

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,25 +1,99 @@
 # ~/.config/fish/config.fish
+# autoalias
+alias aliasy="nvim ~/.config/fish/config.fish"
 
 ### ADDING TO THE PATH
 # First line removes the path; second line sets it.  Without the first line,
 # your path gets massive and fish becomes very slow.
 set -e fish_user_paths
-set -U fish_user_paths $HOME/.local/bin $HOME/Applications  $fish_user_paths
-
-# doom emacs
-alias doom='/home/ps/.config/emacs/bin/doom'
+set -U fish_user_paths $HOME/.local/bin $HOME/Applications $fish_user_paths
 
 # add to easy using bash-scripts
 set -U fish_user_paths /home/ps/Documents/bash-scripts/ $fish_user_paths
+
+
+### Aliasy ###
+
+# Updates for Linux basic commmands
+# Colorize grep output (good for log files)
+alias grep='grep --color=auto'
+alias egrep='egrep --color=auto'
+alias fgrep='fgrep --color=auto'
+
+# confirm before overwriting something
+alias cp="cp -i"
+alias mv='mv -i'
+alias rm='rm -i'
+
+# Changing "ls" to "exa"
+alias l='exa -al --color=always --group-directories-first' # my preferred listing
+alias lt='exa -aT --color=always --group-directories-first' # tree listing
+
+# Shortcuts
+
+alias .="pwd"
+alias ..="cd .."
+alias ...="cd ../.."
+
+alias h='history'
+alias v='nvim'
+alias syc='systemctl'
+alias syd='systemd'
+alias vpn='nmcli connection up'
+
+alias timer='termdown'
+
+# docker
+alias dimg='docker ps --format "table {{.Names}}\t{{.Image}}"'
+alias dport='docker ps --format "table {{.Names}} \t {{.Ports}}"'
+alias dc='docker-compose'
+
+# git
+alias gs='git status'
+alias gl='git log'
+alias ga='git add'
+alias gaf='git add --force'
+alias gb='git branch'
+alias gch='git checkout'
+alias gcl='git clone'
+alias gcm='git commit -m'
+alias gf='git fetch'
+alias tag='git tag'
+alias newtag='git tag -a'
+alias gp='git push origin'
+
+### distro specyfic configuration (package managers)
+# Arch-based only
+# get fastest pacman mirrors
+alias mirror="sudo reflector -f 15 -l 15 --number 10 --verbose --save /etc/pacman.d/mirrorlist"
+alias installed="pacman -Qn | fzf"
+alias installed-yay="pacman -Qm"
+
+# DEBIAN-based only
+#alias update='sudo apt update -y'
+#alias upgrade='sudo apt update -y && sudo apt upgrade -y'
+#alias autoremove='sudo apt autoremove'
+
+# inne
+alias elo='echo "no elo"'
+
+# Config .cfg
+alias config='/usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME'
+###config config --local status.showUntrackedFiles no
+
+### terminal settings
+
+# doom emacs
+alias doom='/home/ps/.config/emacs/bin/doom'
 
 # ranger settings
 set -x RANGER_LOAD_DEFAULT_RC 0
 
 ### EXPORT ###
-set fish_greeting              # Supresses fish's intro message
-set TERM "xterm-256color"      # Sets the terminal type
-set EDITOR "nivm"              # $EDITOR use neovim in terminal
-set VISUAL "code"              # $VISUAL use VScode in GUI mode
+set fish_greeting # Supresses fish's intro message
+set TERM xterm-256color # Sets the terminal type
+set EDITOR nivm # $EDITOR use neovim in terminal
+set VISUAL code # $VISUAL use VScode in GUI mode
 
 ### AUTOCOMPLETE AND HIGHLIGHT COLORS
 set fish_color_normal brcyan
@@ -84,74 +158,3 @@ set -g fish_pager_color_secondary_background
 set -g fish_pager_color_secondary_prefix $cyan
 set -g fish_pager_color_secondary_completion $foreground
 set -g fish_pager_color_secondary_description $comment
-
-### Aliasy ###
-
-## linux fs ##
-# Colorize grep output (good for log files)
-alias grep='grep --color=auto'
-alias egrep='egrep --color=auto'
-alias fgrep='fgrep --color=auto'
-
-# confirm before overwriting something
-alias cp="cp -i"
-alias mv='mv -i'
-alias rm='rm -i'
-
-# Changing "ls" to "exa"
-alias l='exa -al --color=always --group-directories-first' # my preferred listing
-alias lt='exa -aT --color=always --group-directories-first' # tree listing
-
-#cd
-alias .="pwd"
-alias ..="cd .."
-alias ...="cd ../.."
-#history
-alias h='history'
-## apps ##
-# docker
-alias dimg='docker ps --format "table {{.Names}}\t{{.Image}}"'
-alias dport='docker ps --format "table {{.Names}} \t {{.Ports}}"'
-alias dc='docker-compose'
-
-# git
-alias gs='git status'
-alias gl='git log'
-alias ga='git add'
-alias gaf='git add --force'
-alias gb='git branch'
-alias gch='git checkout'
-alias gcl='git clone'
-alias gcm='git commit -m'
-alias gf='git fetch'
-alias tag='git tag'
-alias newtag='git tag -a'
-alias gp='git push origin'
-
-# neovim 
-alias v='nvim'
-
-# timer
-alias timer='termdown'
-
-### distro specyfic configuration (package managers)
-# Arch-based only
-# get fastest pacman mirrors
-alias mirror="sudo reflector -f 15 -l 15 --number 10 --verbose --save /etc/pacman.d/mirrorlist"
-
-# DEBIAN-based only
-#alias update='sudo apt update -y'
-#alias upgrade='sudo apt update -y && sudo apt upgrade -y'
-#alias autoremove='sudo apt autoremove'
-
-# inne
-alias elo='echo "no elo"'
-
-alias syc='systemctl'
-alias syd='systemd'
-
-alias vpn='nmcli connection up'
-
-# Cofig .cfg
-alias config='/usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME'
-###config config --local status.showUntrackedFiles no
